cedafd94689c9bffedff6feb14d5a97a6cea26a7
hernanbravo <hbravog24@gmail.com>
2018-04-17 00:23:46 -0300

    se agrega RecyclerView para bodegas

--- app/src/main/java/adapter/WareHouseAdapter.java
+++ app/src/main/java/adapter/WareHouseAdapter.java
@@ -1,4 +1,4 @@
-package adapters;
+package adapter;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
--- app/src/main/java/layout/InventoryAndWarhouse/Warehouse/WareHouseView.java
+++ app/src/main/java/layout/InventoryAndWarhouse/Warehouse/WareHouseView.java
@@ -2,14 +2,69 @@ package layout.InventoryAndWarhouse.Warehouse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.thesis.inventory.inventoryandroid.R;
+import org.json.JSONArray;
+import org.json.JSONException;
+import java.util.ArrayList;
+import java.util.List;
+import Generics.Globals;
+import adapter.WareHouseAdapter;
+import dto.AlmacenDTO;
+import services.AppController;
 
 public class WareHouseView extends AppCompatActivity {
+
+    private RecyclerView reyclerViewWareHouse;
+    private RecyclerView.Adapter mAdapter;
+    Globals globals = new Globals();
+    List<AlmacenDTO> listProduct;
+    List<AlmacenDTO> listWareHouse = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inventory_warehouse_view);
+
+        reyclerViewWareHouse = (RecyclerView) findViewById(R.id.reyclerViewWareHouse);
+        reyclerViewWareHouse.setHasFixedSize(true);
+        reyclerViewWareHouse.setLayoutManager(new LinearLayoutManager(this));
+        mAdapter = new WareHouseAdapter(GetAllWareHouse());
+        reyclerViewWareHouse.setAdapter(mAdapter);
+    }
+
+    public List<AlmacenDTO> GetAllWareHouse()
+    {
+        String url = globals.getIp() + "InventoryRest/rs/service/getAllWarehouse";
+        listProduct = new  ArrayList<>();
+        JsonArrayRequest jreq = new JsonArrayRequest(url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response)
+                    {
+                        if(response.length() > 0)
+                        {
+                            for(int i =0;i < response.length();i++)
+                            {
+                                try
+                                {
+                                    listWareHouse.add(new AlmacenDTO(Integer.parseInt(response.getJSONObject(i).get("id").toString()),response.getJSONObject(i).get("warehouse").toString()));
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });
+        AppController.getInstance().addToRequestQueue(jreq, "jreq");
+        return listWareHouse;
     }
 }
